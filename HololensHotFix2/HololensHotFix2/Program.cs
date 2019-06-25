69160baae11d6542ed5eb508dea97a339c59a574
Graham Reeves <graham@grahamreeves.com>
2019-06-25 23:59:30 +0100

    Working app. arg0 is ip.

---
+++ HololensHotFix2/HololensHotFix2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Tools.Deploy;
+using System.Net;
+
+namespace HololensHotFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            RemoteDeployClient client = RemoteDeployClient.CreateRemoteDeployClient();
+            client.Connect(new ConnectionOptions()
+            {
+                Credentials = new NetworkCredential("DevToolsUser", string.Empty),
+                IPAddress = IPAddress.Parse(args[0])
+            });
+            client.RemoteDevice.DeleteFile(@"C:\Data\Users\DefaultAccount\AppData\Local\DevelopmentFiles\VSRemoteTools\x86\TailoredDeploy.exe");
+
+        }
+    }
+}
